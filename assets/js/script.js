0c865b946bd1a6947d46399b0df5d4f2d1c6258c
Neddley <dr.turrys@gmail.com>
2023-04-25 14:38:12 -0400

    array para another funcionando correctamente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,51 @@
-const name = document.getElementById('name')
-const level = document.getElementById('level')
-const img = document.getElementById('img')
+const name = document.getElementById("name");
+const level = document.getElementById("level");
+const img = document.getElementById("img");
+const nameArray = []
 
-const searchDigimon = event => {
-    event.preventDefault();
-    const { value } = event.target.digimon;
+fetch("https://digimon-api.vercel.app/api/digimon/", {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+})
+  .then((resp) => resp.json())
+  .then((resp) => resp.forEach(element => {
+	const newName = element.name;
+    nameArray.push(newName);
+    console.log(nameArray);
+  }))
 
-	let url = 'https://digimon-api.vercel.app/api/digimon/name/'+ value;
+const searchDigimon = (event) => {
+  event.preventDefault();
+  const { value } = event.target.digimon;
 
-	fetch(url,{
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-		},
-	})
-	.then(response => response.json())
-	.then(response => renderDigimonData(response))
-    .catch(error => renderNotFound(error));
+  let url = "https://digimon-api.vercel.app/api/digimon/name/" + value;
 
-}
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => renderDigimonData(response))
+    .catch((error) => renderNotFound(error));
+};
 
 const renderDigimonData = (response) => {
-	console.log(response);
-	let objt = response[0];
-	console.log(objt);
-	const name1 = objt.name;
-	console.log(name)
-    const sprite =  objt.img;
-    const level1 = objt.level;
-	console.log(level)
+  let objt = response[0];
+  const name1 = objt.name;
+  const sprite = objt.img;
+  const level1 = objt.level;
 
-    name.innerHTML = name1.toUpperCase();
-    img.setAttribute('src', sprite);
-	level.innerHTML = level1.toUpperCase();
+  name.innerHTML = name1.toUpperCase();
+  img.setAttribute("src", sprite);
+  level.innerHTML = level1.toUpperCase();
 };
 
 const renderNotFound = (error) => {
-    name.innerHTML = 'UNKNOWN';
-    img.setAttribute('src', '/assets/img/unknownEntity.png');
-    level.innerHTML = 'UNKNOWN' ;
+  name.innerHTML = "UNKNOWN1";
+  img.setAttribute("src", "assets/img/unknownEntity.png");
+  level.innerHTML = "UNKNOWN1";
 };
-
