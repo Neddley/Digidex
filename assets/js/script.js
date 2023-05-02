b63170735eb32fa2196635893db0673d7c5ebb44
Neddley <dr.turrys@gmail.com>
2023-05-02 14:45:57 -0400

    Lista terminada, falta responsibilidad

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 const name = document.getElementById("name");
 const level = document.getElementById("level");
+const imgDiv = document.getElementById("data-img")
 const img = document.getElementById("img");
-const imgA1 = document.getElementById("img-another1");
+/* const imgA1 = document.getElementById("img-another1");
 const imgA2 = document.getElementById("img-another2");
-const imgA3 = document.getElementById("img-another3");
-const nameArray = [];
+const imgA3 = document.getElementById("img-another3"); 
+const nameArray = [];*/
+var contenido = document.querySelector("#contenido");
+const popupName = document.getElementById("popup-name")
+const popupImg = document.getElementById("popup-img")
 
-fetch("https://digimon-api.vercel.app/api/digimon/", {
+/* fetch("https://digimon-api.vercel.app/api/digimon/", {
   method: "GET",
   headers: {
     Accept: "application/json",
@@ -35,15 +39,15 @@ function nameGiver(arr, num){
   console.log('arr: ', arr);
   console.log(Array.isArray(arr))
   console.log(typeof(arr))
-  /*
+  
   dato1 = arr[num[0]]
   dato2 = arr[num[1]]
   dato3 = arr[num[2]]
-  console.log(dato1, dato2, dato3)*/
+  console.log(dato1, dato2, dato3)
 }
 
 anothers = Randomnazer();
-names = nameGiver(nameArray,anothers)
+names = nameGiver(nameArray,anothers)*/
 
 const searchDigimon = (event) => {
   event.preventDefault();
@@ -71,6 +75,8 @@ const renderDigimonData = (response) => {
   name.innerHTML = name1.toUpperCase();
   img.setAttribute("src", sprite);
   level.innerHTML = level1.toUpperCase();
+  popupName.innerHTML = name1.toUpperCase();
+  popupImg.setAttribute("src", sprite);
 };
 
 const renderNotFound = (error) => {
@@ -78,3 +84,28 @@ const renderNotFound = (error) => {
   img.setAttribute("src", "assets/img/unknownEntity.png");
   level.innerHTML = "UNKNOWN1";
 };
+
+fetch("https://digimon-api.vercel.app/api/digimon")
+  .then((response) => response.json())
+  .then((datos) => {
+    tabla(datos);
+  });
+
+function tabla(datos) {
+  contenido.innerHTML = "";
+
+  for (let temp of datos) {
+    contenido.innerHTML += `
+    <tr>
+      <th scope="row"> 
+      <td>${temp.name}</td>
+      <td><img src="${temp.img}"></td>
+      <td id="table3">${temp.level}</td>
+    </tr>
+    `;
+  }
+}
+
+function togglePopup(){
+  document.getElementById("popup-1").classList.toggle("active")
+}
